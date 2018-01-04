e6c8cf1879d793459bcb78704b4dc4b64783b72f
CR <rajshekhar@protonmail.com>
2018-01-04 02:13:25 -0600

    pakages and sample route created..but component,action,reducers folders have no files..need to  create to get output

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter,Route} from 'react-router-dom';
+
+import App from './componenets/app';
+
+
+const createStoreWithMiddleware=applyMiddleware()(createStore);
+class Hello extends React.Component{
+    render(){return <div>Hello!</div>}
+}
+class Goodbye extends React.Component{
+    render(){return <div>Goodbye!</div>}
+}
+ReactDOM.render(
+<Provider store={createStoreWithMiddleware(reducers)}>
+<BrowserRouter>
+<div>
+<Route path="/hello" component={Hello}/>
+<Route path="/goodbye" component={Goodbye}/>
+</div>
+</BrowserRouter>
+</Provider>, document.querySelector('.container'));
+
